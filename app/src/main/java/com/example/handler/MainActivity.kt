bc86e904d8cf1a34af09e29dd93b93c1aeab9c28
WINDOWS 10 <mirzayevshaxzod9@gmail.com>
2022-06-06 18:21:22 +0500

    qq

--- app/src/main/java/com/example/handler/MainActivity.kt
+++ app/src/main/java/com/example/handler/MainActivity.kt
@@ -6,59 +6,29 @@ import android.util.Log
 import android.view.View
 import com.example.handler.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),View.OnClickListener {
-    lateinit var binding: ActivityMainBinding
-    private var counter = 0
-    private var time: Long = 0L
-    private lateinit var handler: Handler
-    private  var closeTime:Long=0L
+class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+    private lateinit var handler:Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         handler = Handler(Looper.getMainLooper())
-        if (time == 0L) {
-            time = SystemClock.uptimeMillis()
-            handler.removeCallbacks(runnable)
-            handler.postDelayed(runnable,100)
-        }
-        binding.button.setOnClickListener(this)
+        handler.postDelayed(runnable,1000)
 
 
     }
 
-    private var runnable = object : Runnable {
+    private var runnable=object :Runnable{
         override fun run() {
-            val start=time
-            val millis=SystemClock.uptimeMillis()-start
-            var second=millis/1000
-            val minute=second/60
-            second %=60
-            binding.timeTv.text="$minute:${String.format("%02d",second)}"
-            handler.postDelayed(this,200)
+            binding.progressBar.progress=binding.progressBar.progress+2
+            Thread.sleep(1000)
+            handler.postDelayed(this,1000)
+            }
 
         }
 
     }
 
-    override fun onPause() {
-        super.onPause()
-        handler.removeCallbacks(runnable)
-        closeTime=SystemClock.uptimeMillis()-time
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        time=SystemClock.uptimeMillis()-closeTime
-        handler.postDelayed(runnable,100)
-
-    }
-
-    override fun onClick(p0: View?) {
-            binding.counterTv.text=(++counter).toString()
-    }
-}
